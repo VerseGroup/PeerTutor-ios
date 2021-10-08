b4b5cf39d30f78dccb1957d2a8788dba84a2b17e
Steven Yu <stevenyuser@gmail.com>
2021-10-08 15:28:34 -0400

    Initial Commit

---
+++ PeerTutor/PeerTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeerTutorApp.swift
+//  PeerTutor
+//
+//  Created by Steven Yu on 10/8/21.
+//
+
+import SwiftUI
+
+@main
+struct PeerTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
